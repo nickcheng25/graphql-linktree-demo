20563e1daa43ead0e9ee47e8cfded8b4ae48a313
nickchengunsw <nicholas.cheng@servicerocket.com>
2022-08-10 15:36:06 +1000

    linksearch has show links

--- src/graphql/resolvers/all-links/queries.ts
+++ src/graphql/resolvers/all-links/queries.ts
@@ -1,21 +1,22 @@
-import { AllLinksArgs } from '../../types/types';
 import MusicLink from '../../../db/models/music-link'
 import RegularLink from '../../../db/models/regular-link';
+import ShowLink from '../../../db/models/show-link';
+import { QueryAllLinksByUserIdArgs } from 'src/graphql/types/types';
 
 const allLinkQueries = {
     allLinks: async () => {
         const musicLinks = await MusicLink.find();
         const regularLinks = await RegularLink.find();
+        const showLinks = await ShowLink.find();
         console.log("allLinks: ", [...musicLinks, ...regularLinks]);
-        return [...musicLinks, ...regularLinks];
+        return [...musicLinks, ...regularLinks, ...showLinks];
     },
-    allLinksByUserId: async (_parent: any, args: AllLinksArgs) => {
+    allLinksByUserId: async (_parent: any, args: QueryAllLinksByUserIdArgs) => {
         // update this to user the userId from Args
         const musicLinks = await MusicLink.find({ userId: args.userId });
         const regularLinks = await RegularLink.find({ userId: args.userId });
-        console.log("args.userId: ", args.userId);
-        console.log("allLinksByUserId: ", [...musicLinks, ...regularLinks]);
-        return [...musicLinks, ...regularLinks];
+        const showLinks = await ShowLink.find({ userId: args.userId });
+        return [...musicLinks, ...regularLinks, ...showLinks];
     },
 }
 
